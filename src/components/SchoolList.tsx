45d214ab206ef4d57e0fb5df9399443e434ac5a5
Rogério Menezes <roger@rogermenezes.com>
2021-10-04 21:12:35 +1300

    google places autocomplete api

--- src/components/SchoolList.tsx
+++ src/components/SchoolList.tsx
@@ -32,7 +32,7 @@ interface Props {}
 
 const SchoolList = (props: Props) => {
   return (
-    <div className=" w-full lg:w-4/12 dark:bg-gray-900  bg-white xl:flex">
+    <div className=" w-full lg:w-4/12 dark:bg-gray-900  bg-white xl:flex border border-gray-200">
       <div className="lg:min-w-0 lg:flex-1 ">
         <div className="pl-4 pr-6 pt-4 pb-4 border-b border-t border-gray-200 sm:pl-6 lg:pl-8 xl:pl-6 xl:pt-6 xl:border-t-0">
           <div className="flex items-center">
--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,28 +1,101 @@
-import React from 'react'
+import React, { ChangeEvent } from "react";
 import {
-    Combobox,
-    ComboboxInput,
-    ComboboxPopover,
-    ComboboxList,
-    ComboboxOption,
-  } from "@reach/combobox";
-  import "@reach/combobox/styles.css";
-  import { useGoogleMapsScript, Libraries } from "use-google-maps-script";
-  import usePlacesAutocomplete, {
-    getGeocode,
-    getLatLng,
-  } from "use-places-autocomplete";
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
+import { useGoogleMapsScript, Libraries } from "use-google-maps-script";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
 
-interface Props {
-    
+interface ISearchBoxProps {
+  onSelectAddress: (
+    address: string,
+    latitude: number | null,
+    longitude: number | null
+  ) => void;
+  defaultValue: string;
 }
 
-const SearchBox = (props: Props) => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const libraries: Libraries = ["places"];
 
-export default SearchBox
+const SearchBox = ({ onSelectAddress, defaultValue }: ISearchBoxProps) => {
+  const { isLoaded, loadError } = useGoogleMapsScript({
+    googleMapsApiKey: "AIzaSyDGI5VO_gI08wXWos33Co8pjVAuuntDXDo" ?? "",
+    libraries,
+  });
+
+  if (!isLoaded) return null;
+  if (loadError) return <div>Error loading</div>;
+
+  return (
+    <SearchInput
+      onSelectAddress={onSelectAddress}
+      defaultValue={defaultValue}
+    />
+  );
+};
+
+export default SearchBox;
+
+export const SearchInput = ({
+  onSelectAddress,
+  defaultValue,
+}: ISearchBoxProps) => {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete({ debounce: 500, defaultValue });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+    if (e.target.value === "") {
+      onSelectAddress("", null, null);
+    }
+    console.log(e.target.value);
+  };
+
+  const handleSelect = async (address: string) => {
+    setValue(address, false);
+    clearSuggestions();
+
+    try {
+      const results = await getGeocode({ address });
+      const { lat, lng } = await getLatLng(results[0]);
+      console.log("result", results[0])
+      onSelectAddress(address, lat, lng);
+    } catch (error) {
+      console.error(`😱 Error:`, error);
+    }
+  };
+
+  return (
+    <Combobox openOnFocus onSelect={handleSelect}>
+      <ComboboxInput
+        id="search"
+        value={value}
+        onChange={handleChange}
+        disabled={!ready}
+        placeholder="Type your address..."
+        className="rounded-2xl w-full p-2"
+        autoComplete="off"
+      />
+      <ComboboxPopover>
+        <ComboboxList>
+          {status === "OK" &&
+            data.map(({ place_id, description }) => {
+              return <ComboboxOption key={place_id} value={description} />;
+            })}
+        </ComboboxList>
+      </ComboboxPopover>
+    </Combobox>
+  );
+};
